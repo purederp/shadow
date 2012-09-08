cee58e8b299f6888bd417efc315bb5039dad801a
Joshua O'Sullivan <joshNOSPAMosullivan@gmail.com>
2012-09-08 20:25:45 +1200

    Room generator better

--- roomGen.js
+++ roomGen.js
@@ -1,15 +1,7 @@
-var i = 0;
-var furniture = [];
-var elements = [];
-   
-var backgrounds = [];
-backgrounds[i++] = "./assets/backgrounds/black.jpg";
-backgrounds[i++] = "./assets/backgrounds/blue.jpg";
-backgrounds[i++] = "./assets/backgrounds/green.jpg";
-backgrounds[i++] = "./assets/backgrounds/red.jpg";
-backgrounds[i++] = "./assets/backgrounds/teal.jpg";
-backgrounds[i++] = "./assets/backgrounds/yellow.jpg";   
-i = 0;
+var furnitureDescriptors = [];  //furniture Objects
+var elementDescriptors = [];   //in room Objects
+var backgrounds = [];//Files containing background images
+var rooms = [];      //Room objects
 
 function randomBackground(){
    var bgNum = Math.floor(Math.random()*backgrounds.length);
@@ -45,7 +37,6 @@ function RoomFurniture(item){
    this.xpos = -1;
    this.ypos = -1;
 
-
 }
 
 function RoomElement(item){
@@ -72,77 +63,52 @@ function Room(index){
    }
 }
 
-
-function setUpItems(){
-   furniture[i++] = new RoomFurnitureDescriptor("Rug","",0.5);
-   furniture[i++] = new RoomFurnitureDescriptor("Lamp","",0.5);
-   furniture[i++] = new RoomFurnitureDescriptor("Drawers","",0.5,2);
-   furniture[i++] = new RoomFurnitureDescriptor("Chair","",0.5);
-   furniture[i++] = new RoomFurnitureDescriptor("Bookshelf","",0.5);
-   furniture[i++] = new RoomFurnitureDescriptor("Stool","",0.5);
-   
-   furniture[i++] = new RoomFurnitureDescriptor("Bed","",0.33);
-   furniture[i++] = new RoomFurnitureDescriptor("Coffee Table","",0.33,2);
-   furniture[i++] = new RoomFurnitureDescriptor("Desk","",0.33,2);
-   
-   furniture[i++] = new RoomFurnitureDescriptor("Plant","",0.25);
-   furniture[i++] = new RoomFurnitureDescriptor("Clock","",0.25);
-   
-   furniture[i++] = new RoomFurnitureDescriptor("Couch","",0.1);
-   furniture[i++] = new RoomFurnitureDescriptor("Statue","",0.1);
-   furniture[i++] = new RoomFurnitureDescriptor("Painting","",-1);
-   
-   
-   elements[i++] = new RoomElementDescriptor("Telephone","",0.1);
-   elements[i++] = new RoomElementDescriptor("Cup","",-1);
-   elements[i++] = new RoomElementDescriptor("Globe","",-1);
-   elements[i++] = new RoomElementDescriptor("Desk lamp","",0.1);
-   elements[i++] = new RoomElementDescriptor("TV","",0.1);
-}
-
 /**Need to make all rooms at once, to guarentee that special items will exist someone**/
 function GenRooms(){
    var i,j,k;
    console.log("Genrooms called");
+   
+   //Prepare Backgrounds, furniture
+   setUpBackgrounds();
+   setUpItems();
+   
+   
    //Create a bunch of rooms. number of rooms to create defined in setting.js
-   var rooms = [];
    for(i = 0; i < number_of_rooms; i++){
       rooms[i] = new Room(i);
    }
    
    //Pick which room gets the special items. These are marked with '-1' as the probability
    //furniture
-   for(i = 0; i < furniture.length ; i++){
-      if(furniture[i].probability < 0){
+   for(i = 0; i < furnitureDescriptors.length ; i++){
+      if(furnitureDescriptors[i].probability < 0){
          var roomNum = Math.floor(Math.random() * number_of_rooms);
-         rooms[roomNum].furniture.push(new RoomFurniture(furniture[i]));
-      }
-   }
-   //elements
-   for(i = 0; i < elements.length ; i++){
-      if(elements[i].probability < 0){
-         var roomNum = Math.floor(Math.random() * number_of_rooms);
-         rooms[roomNum].elements.push(new RoomFurniture(furniture[i]));
-         rooms[roomNum].spareElementPlaces--; //one less space for an object to sit.
+         rooms[roomNum].furniture.push(new RoomFurniture(furnitureDescriptors[i]));
       }
    }
    
-   /**If a room has elements, then it needs furniture to support them. Instead of doinf something intelligent, I'll just go through the rooms adding 
-      furniture normally, go through the end, attempt to attahc the 'loose' elements to furniture, and If i can't, then add another table/etc.**/
-      
-   //Place normal furniture**/
+   //elements
+   for(i = 0; i < elementDescriptors.length ; i++){
+      if(elementDescriptors[i].probability < 0){
+         var roomNum = Math.floor(Math.random() * number_of_rooms);
+         rooms[roomNum].elements.push(new RoomElement(elementDescriptors[i]));
+         rooms[roomNum].spareElementPlaces--; //one less space an a desk, etc to sit.
+      }
+   }
+   
+   //Place normal furniture
    for(i = 0; i < number_of_rooms; i++){
-      for(j = 0; j < furniture.length ; j++){
-         if(furniture[j].probability > Math.random()){
-            rooms[i].furniture.push(new RoomFurniture(furniture[j]));
-            rooms[i].spareElementPlaces+=furniture[j].holds;
+      for(j = 0; j < furnitureDescriptors.length ; j++){
+         if(furnitureDescriptors[j].probability > Math.random()){
+            rooms[i].furniture.push(new RoomFurniture(furnitureDescriptors[j]));
+            rooms[i].spareElementPlaces+=furnitureDescriptors[j].holds;
          }
       }
    
       //Should use permututations to walk through the elements array, so we dont' bias toward elements first in the list if we run out of spaces
-      for(j = 0; j < elements.length ; j++){
-         if(elements[j].probability > Math.random() && room[i].spareElementPlaces > 0){
-            rooms[i].elements.push(new RoomElement(elements[j]));
+      for(j = 0; j < elementDescriptors.length ; j++){
+         if(elementDescriptors[j].probability > Math.random() && rooms[i].spareElementPlaces > 0){
+            rooms[i].elements.push(new RoomElement(elementDescriptors[j]));
             rooms[i].spareElementPlaces--;
          }
       }
@@ -158,21 +124,21 @@ function GenRooms(){
          }
       }
    
-      //Should now attached elements to suitable furniture. Note that Elements are NOT given positions byt the room, but, instead by the furniture they sit upon.
+      //Should now attach elements to suitable furniture. Note that Elements are NOT given positions byt the room, but, instead by the furniture they sit upon.
       
       
-      for(j = 0; j < elements.length ; j++){
-         for(k = 0; k < furniture.length; k++){
-            if(rooms[i].furniture[i].attachedElements < furniture[i].maxElements){
-               rooms[i].furniture[i].attachedElements++;
-               rooms[i].furniture[i].elements.push(rooms[i].element[j]);
+      for(j = 0; j < rooms[i].elements.length ; j++){
+         for(k = 0; k < rooms[i].furniture.length; k++){
+            if(rooms[i].furniture[k].attachedElements < rooms[i].furniture[k].maxElements){
+               rooms[i].furniture[k].attachedElements++;
+               rooms[i].furniture[k].elements.push(rooms[i].elements[j]);
                break;
             }         
          }
       }
    
    rooms[i].draw();
-   console.log("Drew room" +i);
+   console.log("Drew room " +i);
    }
 }
 
