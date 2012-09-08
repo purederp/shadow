7f3bfb6364023b09ae1cae60d2468236b81dc032
Joshua O'Sullivan <joshNOSPAMosullivan@gmail.com>
2012-09-09 00:39:24 +1200

    Got room rendering finished

--- itemSetup.js
+++ itemSetup.js
@@ -6,30 +6,39 @@
 /** Furniture and things that sit on furniture**/
 function setUpItems(){
    var i = 0;
-   furnitureDescriptors[i++] = new RoomFurnitureDescriptor("Rug","",0.5);
-   furnitureDescriptors[i++] = new RoomFurnitureDescriptor("Lamp","",0.5);
-   furnitureDescriptors[i++] = new RoomFurnitureDescriptor("Drawers","",0.5,2);
-   furnitureDescriptors[i++] = new RoomFurnitureDescriptor("Chair","",0.5);
-   furnitureDescriptors[i++] = new RoomFurnitureDescriptor("Bookshelf","",0.5);
-   furnitureDescriptors[i++] = new RoomFurnitureDescriptor("Stool","",0.5);
+   //furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Rug","images/furniture/rug0.png",0.5/3);
+   //furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Rug","images/furniture/rug1.png",0.5/3);
+   //furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Rug","images/furniture/rug2.png",0.5/3);
+   furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Lamp","images/furniture/lamp0.png",0.5);
+   furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Drawers","images/furniture/drawer0.png",0.5/3,2);
+   furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Drawers","images/furniture/drawer1.png",0.5/3,2);
+   furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Drawers","images/furniture/drawer2.png",0.5/3,2);
+   furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Chair","images/furniture/chair0.png",0.5/3);
+   furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Chair","images/furniture/chair1.png",0.5/3);
+   furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Chair","images/furniture/chair2.png",0.5/3);
+   furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Bookshelf","images/furniture/bookshelf0.png",0.5);
+   furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Stool","images/furniture/stool0.png",0.5/2);
+   furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Stool","images/furniture/stool1.png",0.5/2);
    
-   furnitureDescriptors[i++] = new RoomFurnitureDescriptor("Bed","",0.33);
-   furnitureDescriptors[i++] = new RoomFurnitureDescriptor("Coffee Table","",0.33,2);
-   furnitureDescriptors[i++] = new RoomFurnitureDescriptor("Desk","",0.33,2);
+   furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Bed","images/furniture/bed.png",0.33);
+   furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Coffee Table","images/furniture/coffee0.png",0.33,2);
+   furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Desk","images/furniture/desk0.png",0.33,2);
    
-   furnitureDescriptors[i++] = new RoomFurnitureDescriptor("Plant","",0.25);
-   furnitureDescriptors[i++] = new RoomFurnitureDescriptor("Clock","",0.25);
+   furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Plant","images/furniture/plant0.png",0.25);
+//   furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Clock","images/furniture/.png",0.25);
    
-   furnitureDescriptors[i++] = new RoomFurnitureDescriptor("Couch","",0.1);
-   furnitureDescriptors[i++] = new RoomFurnitureDescriptor("Statue","",0.1);
-   furnitureDescriptors[i++] = new RoomFurnitureDescriptor("Painting","",-1);
+   furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Couch","images/furniture/sofa0.png",0.1);
+//   furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Statue","images/furniture/.png",0.1);
+   furnitureDescriptors[i++] = new RoomFurnitureDescriptor(20,20,0,"Painting","images/furniture/painting0.png",-1);
    
    i = 0;
-   elementDescriptors[i++] = new RoomElementDescriptor("Telephone","",0.1);
-   elementDescriptors[i++] = new RoomElementDescriptor("Cup","",-1);
-   elementDescriptors[i++] = new RoomElementDescriptor("Globe","",-1);
-   elementDescriptors[i++] = new RoomElementDescriptor("Desk lamp","",0.1);
-   elementDescriptors[i++] = new RoomElementDescriptor("TV","",0.1);
+   elementDescriptors[i++] = new RoomElementDescriptor(10,10,"Telephone","images/elements/phone.png",0.1);
+   elementDescriptors[i++] = new RoomElementDescriptor(10,10,"Cup","images/elements/cup0.png",-1);
+   elementDescriptors[i++] = new RoomElementDescriptor(10,10,"Globe","images/elements/globe0.png",-1);
+   elementDescriptors[i++] = new RoomElementDescriptor(10,10,"Desk lamp","images/elements/lamp0.png",0.1);
+   elementDescriptors[i++] = new RoomElementDescriptor(10,10,"Desk lamp","images/elements/lamp1.png",0.1);
+   elementDescriptors[i++] = new RoomElementDescriptor(10,10,"Desk lamp","images/elements/lamp2.png",0.1);
+   elementDescriptors[i++] = new RoomElementDescriptor(10,10,"TV","images/elements/television0.png",0.1);
 }
 
 function setUpBackgrounds(){
--- settings.js
+++ settings.js
@@ -3,7 +3,8 @@
  var length_of_rooms = 1200;
  var total_length = number_of_rooms * length_of_rooms;
  var height = 300;
+ var worldHeight = height; //Should use this instead, less ambigous
  var viewport_width = 900;
  var player_height = 130;
- var player_width = 50
+ var player_width = 50;
 /**END Game settings**/ 
